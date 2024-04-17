5910b1e9487d7e08bfbb60214d62f8459f7bcf0b
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-04-17 14:44:37 -0500

    Changes to model

--- app/src/main/java/com/example/weightliftingbuddy/models/Workout.kt
+++ app/src/main/java/com/example/weightliftingbuddy/models/Workout.kt
@@ -1,8 +1,35 @@
 package com.example.weightliftingbuddy.models
 
-import java.util.Calendar
+import android.util.Log
+import java.text.SimpleDateFormat
+import java.util.Date
 
-class Workout() {
-    val workoutDate: Calendar? = null
-    val listOfLifts: ArrayList<Lift>? = null
+class Workout(
+    val workoutDate: Date,
+    val listOfExercises: ArrayList<Exercise>
+) {
+
+    companion object {
+        private const val DEFAULT_WORKOUT_DATE_FORMAT = "EEE, dd MMM, YYYY"
+
+        fun getDummyWorkout(): Workout {
+            val listOfExercises = arrayListOf<Exercise>()
+            listOfExercises.add(Exercise.getDummyExercise(4))
+            listOfExercises.add(Exercise.getDummyExercise(6))
+            return Workout(Date(), listOfExercises)
+
+        }
+    }
+
+    fun printInfo(logTag: String = "Luis") {
+        Log.i(logTag, "------ ${getFormattedWorkoutDate()} ------")
+        listOfExercises.forEach {
+            it.printInfo(logTag)
+        }
+    }
+
+    fun getFormattedWorkoutDate(): String {
+        val simpleDateFormat = SimpleDateFormat(DEFAULT_WORKOUT_DATE_FORMAT)
+        return simpleDateFormat.format(workoutDate)
+    }
 }
