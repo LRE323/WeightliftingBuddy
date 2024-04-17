5dddd1da88cb7e9be219a8baaa24ac92fe008d61
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-04-17 15:58:54 -0500

    Setting default selected date to the current date

--- app/src/main/java/com/example/weightliftingbuddy/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/weightliftingbuddy/viewmodels/MainActivityViewModel.kt
@@ -2,10 +2,16 @@ package com.example.weightliftingbuddy.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.weightliftingbuddy.models.Workout
+import java.util.Date
 
 class MainActivityViewModel: ViewModel() {
-    val liveDataWorkoutSelected: MutableLiveData<Workout> by lazy {
-        MutableLiveData<Workout>()
+    val liveDataSelectedDate: MutableLiveData<Date> = MutableLiveData()
+
+    init {
+        liveDataSelectedDate.apply {
+            if (this.value == null) {
+                value = Date()
+            }
+        }
     }
 }
