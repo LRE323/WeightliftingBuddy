1f15031e34513a1812b06c10634f29da511b70d2
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-04-17 13:30:48 -0500

    Changes to model

--- app/src/main/java/com/example/weightliftingbuddy/models/LiftSet.kt
+++ app/src/main/java/com/example/weightliftingbuddy/models/LiftSet.kt
@@ -1,7 +1,27 @@
 package com.example.weightliftingbuddy.models
 
-class LiftSet {
-    val sequentialOrder: Int? = null
-    val repetitions: Int? = null
-    val weight: Double? = null
+import android.util.Log
+import kotlin.random.Random
+
+class LiftSet(
+    var sequentialOrder: Int,
+    var repetitions: Int = 0,
+    var weight: Double = 0.0) {
+
+    companion object {
+        fun buildDummyLiftSet(sequentialOrder: Int): LiftSet {
+            val randomReps = Random.nextInt(5, 12)
+            val randomWeight = Random.nextInt(25, 49) + Random.nextDouble()
+            return LiftSet(sequentialOrder, randomReps, randomWeight)
+        }
+    }
+
+    fun printInfo(logTag: String) {
+        val info = "Set #$sequentialOrder: $repetitions rep(s) at $weight ${getWeightUnit()}"
+        Log.i(logTag, info)
+    }
+
+    private fun getWeightUnit(): String {
+        return "lbs"
+    }
 }
