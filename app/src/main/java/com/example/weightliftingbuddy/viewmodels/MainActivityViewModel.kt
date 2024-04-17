4102b1653bd497bf2642a39387029d811c5d7062
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-04-17 15:28:43 -0500

    LiveData for workout selected

--- app/src/main/java/com/example/weightliftingbuddy/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/weightliftingbuddy/viewmodels/MainActivityViewModel.kt
@@ -1,11 +1,11 @@
 package com.example.weightliftingbuddy.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.weightliftingbuddy.models.Workout
 
 class MainActivityViewModel: ViewModel() {
-
-    fun getToastMessage(): String {
-        return "ViewModel test"
+    val liveDataWorkoutSelected: MutableLiveData<Workout> by lazy {
+        MutableLiveData<Workout>()
     }
-
 }
