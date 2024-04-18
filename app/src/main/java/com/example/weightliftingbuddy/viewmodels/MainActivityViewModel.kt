b6f0182f098c82215de428105603ec4ce484a5a8
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-04-18 11:25:16 -0500

    New ViewModel variable

--- app/src/main/java/com/example/weightliftingbuddy/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/weightliftingbuddy/viewmodels/MainActivityViewModel.kt
@@ -2,6 +2,7 @@ package com.example.weightliftingbuddy.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.weightliftingbuddy.models.Workout
 import java.util.Date
 
 class MainActivityViewModel: ViewModel() {
@@ -9,6 +10,11 @@ class MainActivityViewModel: ViewModel() {
      * The current date that has been set or selected, either by default or by the user
      */
     val liveDataSelectedDate: MutableLiveData<Date> = MutableLiveData()
+
+    /**
+     * The workout that was done on the selected date (liveDataSelectedDate).
+     */
+    val liveDataWorkoutForSelectedDate: MutableLiveData<Workout> = MutableLiveData()
 
     init {
         // Set the value of liveDataSelectedDate to today's date by default.
