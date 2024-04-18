38e0925ee30d1c228226c51e91a76398349da99d
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-04-18 11:48:01 -0500

    Adding logic for selecting workout date

--- app/src/main/java/com/example/weightliftingbuddy/activities/MainActivity.kt
+++ app/src/main/java/com/example/weightliftingbuddy/activities/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.weightliftingbuddy.activities
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
+import android.view.View.OnClickListener
+import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.weightliftingbuddy.GeneralUtilities
 import com.example.weightliftingbuddy.databinding.LayoutHomePageBinding
 import com.example.weightliftingbuddy.viewmodels.MainActivityViewModel
+import java.util.Calendar
 import java.util.Date
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), OnDateSetListener {
     private var viewModel: MainActivityViewModel? = null
     private var binding: LayoutHomePageBinding? = null
 
@@ -42,9 +47,19 @@ class MainActivity : ComponentActivity() {
 
     private fun setOnClickListeners() {
         binding?.apply {
-            workoutDate.setOnClickListener {
-                // Todo...
-            }
+            workoutDate.setOnClickListener(onClickWorkoutDate)
         }
+    }
+
+    private val onClickWorkoutDate: OnClickListener = OnClickListener {
+        val datePickerDialog = DatePickerDialog(this@MainActivity)
+        datePickerDialog.setOnDateSetListener(this)
+        datePickerDialog.show()
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val calendar = Calendar.Builder().build()
+        calendar.set(year, month, dayOfMonth)
+        viewModel?.liveDataSelectedDate?.value = calendar.time
     }
 }
