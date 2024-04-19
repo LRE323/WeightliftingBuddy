390a9bc6aee4ad49438e980ece07c2c762f96932
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-04-19 14:30:01 -0500

    New Activity

---
+++ app/src/main/java/com/example/weightliftingbuddy/activities/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.weightliftingbuddy.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.weightliftingbuddy.R
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
