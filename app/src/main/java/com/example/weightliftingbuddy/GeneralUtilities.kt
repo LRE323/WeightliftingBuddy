f0189ec5311b9f337e0227c52b59b0faa31fefad
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-04-19 10:47:01 -0500

    Changing date format

--- app/src/main/java/com/example/weightliftingbuddy/GeneralUtilities.kt
+++ app/src/main/java/com/example/weightliftingbuddy/GeneralUtilities.kt
@@ -6,7 +6,7 @@ import java.util.Date
 class GeneralUtilities {
 
     companion object {
-        private const val DEFAULT_WORKOUT_DATE_FORMAT = "EEE MMM dd"
+        private const val DEFAULT_WORKOUT_DATE_FORMAT = "EEE MMM dd, YYYY"
         fun getFormattedWorkoutDate(workoutDate: Date): String {
             val simpleDateFormat = SimpleDateFormat(DEFAULT_WORKOUT_DATE_FORMAT)
             return simpleDateFormat.format(workoutDate)
