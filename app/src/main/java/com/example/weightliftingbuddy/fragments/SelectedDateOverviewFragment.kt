266da9137407af34b391bef705d1d90160d65465
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-04-22 10:14:41 -0500

    Moving screen to fragment

--- app/src/main/java/com/example/weightliftingbuddy/fragments/SelectedDateOverviewFragment.kt
+++ app/src/main/java/com/example/weightliftingbuddy/fragments/SelectedDateOverviewFragment.kt
@@ -1,33 +1,32 @@
 package com.example.weightliftingbuddy.fragments
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.weightliftingbuddy.GeneralUtilities
 import com.example.weightliftingbuddy.R
+import com.example.weightliftingbuddy.activities.HomeActivity
+import com.example.weightliftingbuddy.databinding.LayoutSelectedWorkoutOverviewBinding
+import com.example.weightliftingbuddy.viewmodels.SelectedWorkoutDateOverviewViewModel
+import java.util.Calendar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SelectedDateOverviewFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SelectedDateOverviewFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SelectedDateOverviewFragment : Fragment(), OnDateSetListener {
+    private var viewModel: SelectedWorkoutDateOverviewViewModel? = null
+    private var binding: LayoutSelectedWorkoutOverviewBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        viewModel = ViewModelProvider(this)[SelectedWorkoutDateOverviewViewModel::class.java]
+        binding = LayoutSelectedWorkoutOverviewBinding.inflate(layoutInflater)
     }
 
     override fun onCreateView(
@@ -35,26 +34,54 @@ class SelectedDateOverviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_selected_date_overview, container, false)
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SelectedDateOverviewFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initObservers()
+        setOnClickListeners()
+    }
+
+    private fun initObservers() {
+        viewModel?.apply {
+            liveDataSelectedDate.observe(viewLifecycleOwner, onDateSelected)
+        }
+    }
+
+    private val onDateSelected = Observer<Calendar> {
+        binding?.apply {
+            workoutDate.text = GeneralUtilities.getFormattedWorkoutDate(it.time)
+        }
+    }
+
+    private fun setOnClickListeners() {
+        binding?.apply {
+            workoutDate.setOnClickListener(onClickWorkoutDate)
+            noWorkoutLoggedView.buttonAddNewActivity.setOnClickListener {
+                Toast.makeText(requireContext(), "Click", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private val onClickWorkoutDate: View.OnClickListener = View.OnClickListener {
+        val selectedDate = viewModel?.liveDataSelectedDate?.value
+        selectedDate?.apply {
+            showWorkoutDatePickerDialog(this)
+        }
+    }
+
+    private fun showWorkoutDatePickerDialog(selectedDate: Calendar) {
+        val year = selectedDate.get(Calendar.YEAR)
+        val monthOfYear = selectedDate.get(Calendar.MONTH)
+        val dayOfMonth = selectedDate.get(Calendar.DAY_OF_MONTH)
+        val datePickerDialog = DatePickerDialog(requireContext(), 0, this, year, monthOfYear, dayOfMonth)
+        datePickerDialog.show()
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val calendar = Calendar.Builder().build()
+        calendar.set(year, month, dayOfMonth)
+        viewModel?.liveDataSelectedDate?.value = calendar
     }
 }
