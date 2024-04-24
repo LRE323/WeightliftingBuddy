a2f50a1bae08cb601b0bab64b29750c637f354cc
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-04-24 15:04:36 -0500

    Imports

--- app/src/main/java/com/example/weightliftingbuddy/fragments/SelectedDateOverviewFragment.kt
+++ app/src/main/java/com/example/weightliftingbuddy/fragments/SelectedDateOverviewFragment.kt
@@ -2,19 +2,16 @@ package com.example.weightliftingbuddy.fragments
 
 import android.app.DatePickerDialog
 import android.app.DatePickerDialog.OnDateSetListener
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.weightliftingbuddy.GeneralUtilities
-import com.example.weightliftingbuddy.R
-import com.example.weightliftingbuddy.activities.HomeActivity
 import com.example.weightliftingbuddy.databinding.LayoutSelectedWorkoutOverviewBinding
 import com.example.weightliftingbuddy.viewmodels.SelectedWorkoutDateOverviewViewModel
 import java.util.Calendar
