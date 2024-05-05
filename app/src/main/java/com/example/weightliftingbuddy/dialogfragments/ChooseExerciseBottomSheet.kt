b8de33022284c25d3a9c0833900cc487e7c8c4c4
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-05-05 11:31:01 -0500

    Adding constructor value

--- app/src/main/java/com/example/weightliftingbuddy/dialogfragments/ChooseExerciseBottomSheet.kt
+++ app/src/main/java/com/example/weightliftingbuddy/dialogfragments/ChooseExerciseBottomSheet.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.weightliftingbuddy.R
+import com.example.weightliftingbuddy.models.Exercise
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class ChooseExerciseBottomSheet: BottomSheetDialogFragment() {
+class ChooseExerciseBottomSheet(val exerciseList: List<Exercise>): BottomSheetDialogFragment() {
+
+    companion object {
+        const val TAG = "CHOOSE_EXERCISE_BOTTOM_SHEET"
+    }
 
 
     override fun onCreateView(
@@ -16,9 +21,5 @@ class ChooseExerciseBottomSheet: BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.bottom_sheet_choose_exercise, container)
-    }
-
-    companion object {
-        const val TAG = "CHOOSE_EXERCISE_BOTTOM_SHEET"
     }
 }
