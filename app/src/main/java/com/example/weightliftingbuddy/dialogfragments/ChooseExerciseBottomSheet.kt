4463a4a10bc4fd0163c6c7fa3771579b974a1bfe
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-05-08 17:21:36 -0500

    ChooseExerciseBottomSheet

--- app/src/main/java/com/example/weightliftingbuddy/dialogfragments/ChooseExerciseBottomSheet.kt
+++ app/src/main/java/com/example/weightliftingbuddy/dialogfragments/ChooseExerciseBottomSheet.kt
@@ -4,16 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weightliftingbuddy.ExerciseListAdapter
 import com.example.weightliftingbuddy.R
+import com.example.weightliftingbuddy.databinding.BottomSheetChooseExerciseBinding
 import com.example.weightliftingbuddy.models.Exercise
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
+/*
+TODO: Cannot use this class until I find out why onCreateViewHolder is not being called in the Adapter
+ */
 class ChooseExerciseBottomSheet(val exerciseList: List<Exercise>): BottomSheetDialogFragment() {
+    private var binding: BottomSheetChooseExerciseBinding? = null
+
+    // RecyclerView stuff
+    private var recyclerViewExerciseList: RecyclerView? = null
+    private var adapterExerciseList: ExerciseListAdapter? = null
 
     companion object {
         const val TAG = "CHOOSE_EXERCISE_BOTTOM_SHEET"
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = BottomSheetChooseExerciseBinding.inflate(layoutInflater)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,5 +37,19 @@ class ChooseExerciseBottomSheet(val exerciseList: List<Exercise>): BottomSheetDi
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.bottom_sheet_choose_exercise, container)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        adapterExerciseList = ExerciseListAdapter(exerciseList, null)
+        recyclerViewExerciseList = binding?.chooseExerciseBottomSheetList
+        recyclerViewExerciseList?.apply {
+            adapter = adapterExerciseList
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 }
