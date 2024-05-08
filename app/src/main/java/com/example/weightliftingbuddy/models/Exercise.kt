1d4a3936e5985eb2de271ce0a9f6d71067135f57
LuisRuiz <luis.e.ruiz1209@gmail.com>
2024-05-08 18:12:13 -0500

    Extending Parcelable

--- app/src/main/java/com/example/weightliftingbuddy/models/Exercise.kt
+++ app/src/main/java/com/example/weightliftingbuddy/models/Exercise.kt
@@ -1,10 +1,32 @@
 package com.example.weightliftingbuddy.models
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
 data class Exercise(
     @PrimaryKey
-    var exerciseName: String) {
+    var exerciseName: String): Parcelable {
+    constructor(parcel: Parcel) : this(parcel.readString().toString()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(exerciseName)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Exercise> {
+        override fun createFromParcel(parcel: Parcel): Exercise {
+            return Exercise(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Exercise?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
