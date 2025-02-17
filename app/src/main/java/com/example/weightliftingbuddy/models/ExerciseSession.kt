946a91c6a7f76bf3d55c3c0a76da648561782cc8
Luis Ruiz <luis.e.ruiz1209@gmail.com>
2025-02-16 19:18:23 -0600

    Removing variable

--- app/src/main/java/com/example/weightliftingbuddy/models/ExerciseSession.kt
+++ app/src/main/java/com/example/weightliftingbuddy/models/ExerciseSession.kt
@@ -11,7 +11,7 @@ class ExerciseSession(
         fun getDummyExercise(amountOfSets: Int): ExerciseSession {
             val exerciseSets: ArrayList<ExerciseSet> = arrayListOf()
             for (i in 1..amountOfSets) {
-                val newLift = ExerciseSet.buildDummyLiftSet(i)
+                val newLift = ExerciseSet.buildDummyLiftSet()
                 exerciseSets.add(newLift)
             }
             return ExerciseSession(Exercise("Squat"), exerciseSets)
--- app/src/main/java/com/example/weightliftingbuddy/models/ExerciseSet.kt
+++ app/src/main/java/com/example/weightliftingbuddy/models/ExerciseSet.kt
@@ -4,20 +4,20 @@ import android.util.Log
 import kotlin.random.Random
 
 class ExerciseSet(
-    var sequentialOrder: Int,
     var repetitions: Int = 0,
-    var weight: Double = 0.0) {
+    var weight: Double = 0.0
+) {
 
     companion object {
-        fun buildDummyLiftSet(sequentialOrder: Int): ExerciseSet {
+        fun buildDummyLiftSet(): ExerciseSet {
             val randomReps = Random.nextInt(5, 12)
             val randomWeight = Random.nextInt(25, 49) + Random.nextDouble()
-            return ExerciseSet(sequentialOrder, randomReps, randomWeight)
+            return ExerciseSet(randomReps, randomWeight)
         }
     }
 
     fun printInfo(logTag: String) {
-        val info = "Set #$sequentialOrder: $repetitions rep(s) at $weight ${getWeightUnit()}"
+        val info = "$repetitions rep(s) at $weight ${getWeightUnit()}"
         Log.i(logTag, info)
     }
 
