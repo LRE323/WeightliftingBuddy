b0c5c25d4508e70bde4328052ad5c4e220c4561f
Luis Ruiz <ruizl1209@gmail.com>
2025-02-17 11:23:11 -0600

    New DAO function

--- app/src/main/java/com/example/weightliftingbuddy/room/dao/WorkoutDao.kt
+++ app/src/main/java/com/example/weightliftingbuddy/room/dao/WorkoutDao.kt
@@ -4,11 +4,15 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.room.Update
 import com.example.weightliftingbuddy.models.Workout
 
 @Dao
 interface WorkoutDao {
+
+    @Query("SELECT * FROM workout ORDER BY workoutDate")
+    suspend fun getAllWorkouts(): List<Workout>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertWorkout(workout: Workout)
