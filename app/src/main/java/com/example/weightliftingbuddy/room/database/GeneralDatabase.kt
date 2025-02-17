d32627b17d9d2250871c791c5321c2bc09d9d292
Luis Ruiz <ruizl1209@gmail.com>
2025-02-17 00:22:50 -0600

    Renaming

--- app/src/main/java/com/example/weightliftingbuddy/room/database/GeneralDatabase.kt
+++ app/src/main/java/com/example/weightliftingbuddy/room/database/GeneralDatabase.kt
@@ -7,14 +7,14 @@ import com.example.weightliftingbuddy.models.Exercise
 import com.example.weightliftingbuddy.models.ExerciseSet
 import com.example.weightliftingbuddy.room.dao.ExerciseDao
 import com.example.weightliftingbuddy.room.dao.ExerciseSetDao
-import com.example.weightliftingbuddy.room.typeconverter.ExerciseSessionTypeConverters
+import com.example.weightliftingbuddy.room.typeconverter.ExerciseSessionTypeConverter
 import com.example.weightliftingbuddy.room.typeconverter.WorkoutTypeConverter
 
 @Database(
     entities = [Exercise::class, ExerciseSet::class],
     version = 1
 )
-@TypeConverters(ExerciseSessionTypeConverters::class, WorkoutTypeConverter::class)
+@TypeConverters(ExerciseSessionTypeConverter::class, WorkoutTypeConverter::class)
 abstract class GeneralDatabase: RoomDatabase() {
 
     companion object {
--- app/src/main/java/com/example/weightliftingbuddy/room/typeconverter/ExerciseSessionTypeConverter.kt
+++ app/src/main/java/com/example/weightliftingbuddy/room/typeconverter/ExerciseSessionTypeConverter.kt
@@ -3,10 +3,9 @@ package com.example.weightliftingbuddy.room.typeconverter
 import androidx.room.TypeConverter
 import com.example.weightliftingbuddy.models.Exercise
 import com.example.weightliftingbuddy.models.ExerciseSet
-import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
-class ExerciseSessionTypeConverters: BaseTypeConverter() {
+class ExerciseSessionTypeConverter: BaseTypeConverter() {
     @TypeConverter
     fun fromExercise(exercise: Exercise?): String? {
         return exercise?.exerciseName
