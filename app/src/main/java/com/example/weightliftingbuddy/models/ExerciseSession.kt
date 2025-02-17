8f814170902bc5a6fef510aa1ea8285f4b0dd5cb
Luis Ruiz <ruizl1209@gmail.com>
2025-02-16 23:56:13 -0600

    Making it a data class

--- app/src/main/java/com/example/weightliftingbuddy/models/ExerciseSession.kt
+++ app/src/main/java/com/example/weightliftingbuddy/models/ExerciseSession.kt
@@ -1,18 +1,6 @@
 package com.example.weightliftingbuddy.models
 
-class ExerciseSession(
+data class ExerciseSession(
     var exercise: Exercise? = null,
     var listOfExerciseSets: ArrayList<ExerciseSet>? = null) {
-
-    companion object {
-
-        fun getDummyExercise(amountOfSets: Int): ExerciseSession {
-            val exerciseSets: ArrayList<ExerciseSet> = arrayListOf()
-            for (i in 1..amountOfSets) {
-                val newLift = ExerciseSet.buildDummyLiftSet()
-                exerciseSets.add(newLift)
-            }
-            return ExerciseSession(Exercise("Squat"), exerciseSets)
-        }
-    }
 }
