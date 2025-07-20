c199b500deb4df74a4ab5c56651fdfbcb479939a
Luis Ruiz <luis.e.ruiz1209@gmail.com>
2025-07-20 17:04:07 -0500

    Extending Parceable

--- app/src/main/java/com/example/weightliftingbuddy/data/models/ExerciseSession.kt
+++ app/src/main/java/com/example/weightliftingbuddy/data/models/ExerciseSession.kt
@@ -1,13 +1,17 @@
 package com.example.weightliftingbuddy.data.models
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
 
-@Entity
+@Entity @Parcelize
 data class ExerciseSession(
     var exercise: Exercise? = null,
-    var listOfExerciseSets: ArrayList<ExerciseSet>? = null) {
+    var listOfExerciseSets: ArrayList<ExerciseSet>? = null): Parcelable {
 
+    @IgnoredOnParcel
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
 }
--- app/src/main/java/com/example/weightliftingbuddy/data/models/ExerciseSet.kt
+++ app/src/main/java/com/example/weightliftingbuddy/data/models/ExerciseSet.kt
@@ -1,13 +1,15 @@
 package com.example.weightliftingbuddy.data.models
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
-@Entity
+@Entity @Parcelize
 data class ExerciseSet(
     var repetitions: Int = 0,
     var weight: Double = 0.0
-) {
+): Parcelable {
 
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
